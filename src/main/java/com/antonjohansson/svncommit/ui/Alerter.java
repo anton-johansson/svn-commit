a66882bce3dadc056f547abd832a3e61b00e68cc
Anton Johansson <antoon.johansson@gmail.com>
2015-12-28 18:31:19 +0100

    Added JavaDoc

--- src/main/java/com/antonjohansson/svncommit/ui/Alerter.java
+++ src/main/java/com/antonjohansson/svncommit/ui/Alerter.java
@@ -4,10 +4,20 @@ import static javafx.scene.control.Alert.AlertType.ERROR;
 
 import javafx.scene.control.Alert;
 
+/**
+ * Utility class for alerting error messages.
+ *
+ * @author Anton Johansson
+ */
 public final class Alerter
 {
 	private Alerter() {}
 
+	/**
+	 * Alerts given error message.
+	 *
+	 * @param contentText The content to put into the dialog.
+	 */
 	public static void error(String contentText)
 	{
 		Alert alert = new Alert(ERROR);
